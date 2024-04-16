1899a9565fae417869d4fa7d128b548f409c54f3
shktabish <shktab1sh93@gmail.com>
2024-04-17 01:31:25 +0530

    added express.static

--- server/src/app.js
+++ server/src/app.js
@@ -5,11 +5,13 @@ const cookieParser = require("cookie-parser")
 const app = express()
 
 app.use(cors({
-    origin: [process.env.CORS_ORIGIN, process.env.CLOUDINARY_URL],
+    // origin: [process.env.CORS_ORIGIN, process.env.CLOUDINARY_URL],
+    origin: true,
     credentials: true
 }))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(express.static("public"))
 app.use(cookieParser())
 
 //routes import
