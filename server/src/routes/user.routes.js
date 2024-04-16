8f7ea551365a9ce5741b9b52b9b44dd0e37acc6a
shktabish <shktab1sh93@gmail.com>
2024-04-17 01:39:06 +0530

    readded multer

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -5,7 +5,12 @@ const upload = require("../middlewares/multer.middleware")
 
 const router = express.Router()
 
-router.post('/register', registerUser)
+router.post('/register',upload.fields([
+    {
+        name: "avatar",
+        maxCount: 1
+    }
+]) , registerUser)
 router.post('/login', loginUser)
 router.post('/logout', verifyJWT, logoutUser)
 router.post('/refresh-token', refreshAccessToken)
