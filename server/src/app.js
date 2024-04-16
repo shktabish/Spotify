9e42d1f7227290c86f019fe9e3e3ea8dbe0b3955
shktabish <shktab1sh93@gmail.com>
2024-04-17 01:40:08 +0530

    commit

--- server/src/app.js
+++ server/src/app.js
@@ -5,8 +5,7 @@ const cookieParser = require("cookie-parser")
 const app = express()
 
 app.use(cors({
-    // origin: [process.env.CORS_ORIGIN, process.env.CLOUDINARY_URL],
-    origin: true,
+    origin: "https://spotify-nine-tawny.vercel.app",
     credentials: true
 }))
 app.use(express.json())
