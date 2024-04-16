f3b5aaef2a691e08d4f88dd764ac777ad4a3db62
shktabish <shktab1sh93@gmail.com>
2024-04-17 01:46:20 +0530

    multer edit

--- server/src/middlewares/multer.middleware.js
+++ server/src/middlewares/multer.middleware.js
@@ -2,7 +2,7 @@ const multer = require("multer")
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, "./public/temp")
+      cb(null, path.join(process.cwd(), "/public/temp"))
     },
     filename: function (req, file, cb) {
       cb(null, file.originalname)
