57b060973be0b10fcb1b0e4ba751defa52a7a323
shktabish <shktab1sh93@gmail.com>
2024-06-25 15:13:32 +0530

    updated update logic on FE

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -3,8 +3,19 @@ import { CiSearch } from "react-icons/ci"
 import { BiLibrary } from "react-icons/bi"
 import { HiOutlinePlusSmall } from "react-icons/hi2"
 import { IoMdCloudUpload } from "react-icons/io"
+import { useUser } from "../Contexts/UserContext"
 
 const Sidebar = ({ setIsUploadOpen }) => {
+  const { user } = useUser()
+  
+  const handleClick = () => {
+    if(!user) {
+      alert("You need to be logged in to upload songs")
+      return
+    }
+    setIsUploadOpen(true)
+  }
+
   return (
     <div className="h-[calc(100vh-16px)] w-64 flex flex-col gap-2 max-sm:hidden sticky top-2">
       <div className="w-full bg-[#121212] h-1/4 rounded-xl px-4 py-2 flex flex-col gap-2 justify-around">
@@ -13,7 +24,7 @@ const Sidebar = ({ setIsUploadOpen }) => {
           <GoHomeFill className="fill-white text-3xl mr-4 ml-2" />
           <span className="text-white text-l font-semibold">Home</span>
         </div>
-        <div onClick={() => setIsUploadOpen(true)} className="flex items-center cursor-pointer">
+        <div onClick={handleClick} className="flex items-center cursor-pointer">
           <IoMdCloudUpload className="fill-[#b3b3b3] text-3xl mr-4 ml-2" />
           <span className="text-[#b3b3b3] text-l font-semibold">Upload Songs</span>
         </div>
