b89b1a0a2690d3fa0e100559cfbd1cc52566d7f5
shktabish <shktab1sh93@gmail.com>
2024-07-11 19:21:28 +0530

    commit

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client"
 
-const socket = io("http://localhost:8000")
+const socket = io("https://spotify-server-gamma.vercel.app/")
+// const socket = io("http://localhost:8000")
 
 export default socket
